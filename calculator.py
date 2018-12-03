7739c3a5663dbdac677a75c5f5aa419f429baad8
debjohnson33 <debjohnson_11@yahoo.com>
2018-12-03 15:34:32 -0600

    Added simple calculating functions

---
+++ calculator.py
@@ -0,0 +1,18 @@
+def calculator():
+    print("Hey this is my calculator program!")
+
+calculator()
+
+# Calculator functions are: add, subtract, multiply, divide
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
